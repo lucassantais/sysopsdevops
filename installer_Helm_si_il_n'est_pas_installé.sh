69ded8f401d87cb7bab41b38be14acf2edf486e9
lucassantais <154605249+lucassantais@users.noreply.github.com>
2024-04-14 18:05:07 +0200

    Delete installer_Helm_si_il_n'est_pas_installé.sh

--- installer_Helm_si_il_n'est_pas_installé.sh
+++
@@ -1,6 +0,0 @@
-if ! command -v helm &> /dev/null; then
-    echo "Helm n'est pas installé. Installation en cours..."
-    curl -LO https://get.helm.sh/helm-v3.7.0-linux-amd64.tar.gz
-    tar -zxvf helm-v3.7.0-linux-amd64.tar.gz
-    sudo mv linux-amd64/helm /usr/local/bin/helm
-fi
